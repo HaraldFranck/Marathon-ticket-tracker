4988c22cb7148b44e7f8f04e1b0d26b5151728a8
HaraldFranck <harald@franck.nu>
2024-03-08 10:19:44 +0100

    changed time interval

--- src/cph_marathon_tracker.py
+++ src/cph_marathon_tracker.py
@@ -3,19 +3,13 @@ from bs4 import BeautifulSoup
 import subprocess
 
 URL = "https://secure.onreg.com/onreg2/bibexchange/?eventid=6087"
-CHECK_INTERVAL = 2  # in seconds
-EMAIL_SENDER = 'harald@franck.nu'
-EMAIL_RECEIVER = 'harald@franck.nu'
-EMAIL_PASSWORD = 'yourpassword'
 
 def check_tickets():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    print(soup.prettify())
     containers = soup.find_all("b")
 
     for container in containers:
-        print(container.text)
         if "Der er ikke nogen startnumre til salg i øjeblikket. Prøv igen lidt senere." not in container.text:
             return True
     return False
@@ -33,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print(1)
